4244ad59d74fac2d33d77e7e6ffc9f60097ad06a
LYS <skylys54@gmail.com>
2021-10-20 08:41:06 +0800

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import styles from "./index.module.scss";
 // import Robot from "./components/other/Robot";
 // import tsConfig from "./assets/images/tsConfig.jpg";
 // import ShoppingCart from "./components/other/ShoppingCart";
-import { Header, Footer } from './components';
-
+import { BrowserRouter, Route, Switch, HashRouter } from 'react-router-dom';
+import { HomePage } from './pages';
 function App() {
-
+  /* exact 精准匹配路径 */
   return (
     <div className={styles.App}>
-      <Header title={'这是title'}></Header>
-      <Footer></Footer>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/:abc" component={HomePage} />
+          <Route path="/loginIn" render={() => <h1>登录页面</h1>} />
+          {/* 404页面 */}
+          <Route render={() => <h1>404页面</h1>} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,20 @@
 import React, { createRef, ReactElement, useState, useEffect } from "react";
 // css module css in js
-import styles from "./HomePage.module.scss";
+import styles from "./HomePage.module.css";
 import { Header, Footer } from '../../components';
+import {RouteComponentProps, useLocation} from 'react-router-dom';
 
-function HomePage() {
+interface PropsParams {
+  abc: string
+}
+
+export const HomePage:React.FC<RouteComponentProps<PropsParams>> = (props) => {
+  console.log('props', props.match.params.abc);
+  console.log('query:', useLocation().search);
+  const query = new URLSearchParams(useLocation().search);
+  console.log('query', query.get('q'));
+  
+  
   return (
     <div className={styles.App}>
       <Header title={'这是title'}></Header>
@@ -12,4 +23,3 @@ function HomePage() {
   );
 }
 
-export default HomePage;
